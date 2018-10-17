dab17e12a52558d8813e9414b8187ea4b0e71480
Unknown <regoczi.tamas01@gmail.com>
2018-10-17 11:26:29 +0200

    bugfix

--- src/main/java/com/codecool/newsservice/config/DataSourceConfig.java
+++ src/main/java/com/codecool/newsservice/config/DataSourceConfig.java
@@ -1,9 +1,6 @@
 package com.codecool.newsservice.config;
 
-import com.sun.deploy.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
