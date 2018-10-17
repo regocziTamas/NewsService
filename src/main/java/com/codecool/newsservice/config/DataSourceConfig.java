6074520ccef65fdcfbd74c2f52f0ca4fb612780b
Unknown <regoczi.tamas01@gmail.com>
2018-10-17 11:23:37 +0200

    datasource config

--- src/main/java/com/codecool/newsservice/config/DataSourceConfig.java
+++ src/main/java/com/codecool/newsservice/config/DataSourceConfig.java
@@ -1,19 +1,30 @@
 package com.codecool.newsservice.config;
 
+import com.sun.deploy.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+
+
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
+    /*@ConfigurationProperties(prefix = "spring.datasource")*/
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(System.getenv("JDBC_DATABASE_URL"));
+        dataSource.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
+        dataSource.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
+
+        return dataSource;
     }
 }
